d450c678077df5342fe924750667acb6fb159680
dongrui <dongruisohu@gmail.com>
2018-02-05 10:54:32 +0800

    判断编译是否成功

--- start-local-ceph.sh
+++ start-local-ceph.sh
@@ -3,7 +3,13 @@
 
 cd build
 ../src/stop.sh
-make vstart        # builds just enough to run vstart
+rm -rf ./out/*
+make -j7      # builds just enough to run vstart
+if [ $? -ne 0 ]; then
+    echo "make fail"
+    exit 0
+fi
+
 ../src/vstart.sh --debug --new -x --localhost 
 ./bin/ceph -s
 
