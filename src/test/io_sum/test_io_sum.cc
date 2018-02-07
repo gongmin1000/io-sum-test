bc177658e472699839a53e80da92ba1ab99aa6b4
dongrui <dongruisohu@gmail.com>
2018-02-07 10:37:56 +0800

    增加测试MonitorDBStore的代码

--- src/test/io_sum/test_io_sum.cc
+++ src/test/io_sum/test_io_sum.cc
@@ -11,18 +11,44 @@
  * Foundation.  See file COPYING.
  *
  */
+#include "acconfig.h"
+
 
 #include <iostream>
 #include <errno.h>
 
+
+#include "global/global_init.h"
+#include "global/global_context.h"
+#include "common/ceph_argparse.h"
+#include "include/assert.h"
+#include "common/debug.h"
+#include "common/errno.h"
+
+
+
+
+
 #include "include/rados/librados.hpp"
 #include "include/encoding.h"
+
+#include "mon/MonitorDBStore.h"
+
 #include "test/librados/test.h"
 #include "gtest/gtest.h"
 
+
+#include "include/assert.h"
+
+#define dout_context g_ceph_context
+#define dout_subsys ceph_subsys_
+#undef dout_prefix
+#define dout_prefix *_dout << "io-sum-msg "
+
 using namespace librados;
 
-TEST(DrHello, Test01) {
+TEST(IOSum, Test01) {
+  return; 
   Rados cluster;
   std::string pool_name = get_temp_pool_name();
   ASSERT_EQ("", create_one_pool_pp(pool_name, cluster));
@@ -46,31 +72,35 @@ TEST(DrHello, Test01) {
 }
 
 
-/*TEST(DrHello, Test02) {
-  Rados cluster;
-  std::string pool_name = get_temp_pool_name();
-  ASSERT_EQ("", create_one_pool_pp(pool_name, cluster));
-  IoCtx ioctx;
-  cluster.ioctx_create(pool_name.c_str(), ioctx);
-
-  bufferlist in, out;
-  ASSERT_EQ(-ENOENT, ioctx.exec("myobject", "hello", "say_hello", in, out));
-  ASSERT_EQ(0, ioctx.write_full("myobject", in));
-  ASSERT_EQ(0, ioctx.exec("myobject", "hello", "say_hello", in, out));
-  ASSERT_EQ(std::string("Hello, world!"), std::string(out.c_str(), out.length()));
-
-  out.clear();
-  in.append("Tester");
-  ASSERT_EQ(0, ioctx.exec("myobject", "hello", "say_hello", in, out));
-  ASSERT_EQ(std::string("Hello, Tester!"), std::string(out.c_str(), out.length()));
-
-  out.clear();
-  in.clear();
-  char buf[4096];
-  memset(buf, 1, sizeof(buf));
-  in.append(buf, sizeof(buf));
-  ASSERT_EQ(-EINVAL, ioctx.exec("myobject", "hello", "say_hello", in, out));
-
-  ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
+TEST(IOSum, Test02) {
+  MonitorDBStore store(std::string("/home/dongrui/work/ceph-dev/io-sum-test/build"));
+  //在目录下kv_backend内存储kv后端类型，这里使用rocksdb
+  dout(0) ;
+  store.create_and_open(*_dout);
+  
+  MonitorDBStore::TransactionRef t(new MonitorDBStore::Transaction());
+  t->put("test02","x",1);
+  store.apply_transaction(t);
+  *_dout << "x = "  << store.get("test02","x");
+  store.close();
+  *_dout << dendl;
 }
-*/
+
+
+int main(int argc, char *argv[])
+{
+  vector<const char*> args;
+  argv_to_vec(argc, (const char **)argv, args);
+
+  auto cct = global_init(nullptr, args,
+                         CEPH_ENTITY_TYPE_CLIENT, CODE_ENVIRONMENT_UTILITY,
+                         0);
+  common_init_finish(g_ceph_context);
+  g_ceph_context->_conf->apply_changes(NULL);
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
+
+
+
--- start-local-ceph.sh
+++ start-local-ceph.sh
@@ -4,7 +4,7 @@
 cd build
 ../src/stop.sh
 rm -rf ./out/*
-make -j7      # builds just enough to run vstart
+make -j7       # builds just enough to run vstart
 if [ $? -ne 0 ]; then
     echo "make fail"
     exit 0
